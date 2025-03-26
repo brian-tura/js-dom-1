173904be1d4044461c939e83ca168a4291ed6b0e
brian-tura <b.tura2002@gmail.com>
2025-03-26 08:34:30 +0100

    turning off lamp done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const lampadina = document.getElementById('lampadina');
 const bottone = document.getElementById('btn');
 
+bottone.addEventListener('click', function() {
+    if(lampadina.scroll.includes('yellow')){
+        lampadina.src = '../img/white_lamp.png';
+        btn.textContent = 'Accendi';
+    }
+    else {
+        lampadina.src='../img/yellow_lamp.png';
+        
+    }
+})
