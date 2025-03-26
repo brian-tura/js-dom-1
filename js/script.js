35846536c0aceb87a6b011d06605dadc2e248702
brian-tura <b.tura2002@gmail.com>
2025-03-26 08:39:01 +0100

    Excercise done

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ const lampadina = document.getElementById('lampadina');
 const bottone = document.getElementById('btn');
 
 bottone.addEventListener('click', function() {
-    if(lampadina.scroll.includes('yellow')){
+    if(lampadina.src.includes('yellow')){
         lampadina.src = '../img/white_lamp.png';
-        btn.textContent = 'Accendi';
+        bottone.textContent = 'Accendi';
     }
     else {
         lampadina.src='../img/yellow_lamp.png';
-        
+        bottone.textContent = 'Spegni';   
     }
-})
+});
